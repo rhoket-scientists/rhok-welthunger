be969ccddb9816771cd94cab25635e0de3f23f86
Willi <willi@jups42.de>
2012-06-02 21:04:25 +0200

    tried handmade thresholding which oes not work

--- Application.cpp
+++ Application.cpp
@@ -25,18 +25,57 @@ Application::Application(int argc, char *argv[])
 
 
     for(int i = 0; i < imageFiles.size(); i++) {
-        std::cout << imageFiles[i].cols * imageFiles[i].rows << std::endl;
+        //std::cout << imageFiles[i].cols * imageFiles[i].rows << std::endl;
     }
 
 
     for(int i = 0; i < 3; i++) {
         for(int x = 0; x < imageFiles[i].rows; x++) {
-            for(int y = 0; y < imageFiles[i].cols; y++) {
-                out.at<cv::Vec3b>(x,y)[i] = imageFiles[i].at<short>(x,y);
+            for(int x = 0; x < imageFiles[i].cols; x++) {
+                out.at<cv::Vec3b>(x,x)[i] = imageFiles[i].at<short>(x,x);
             }
         }
-        cv::imwrite("outImg.tif", out);
     }
+
+    std::vector<std::vector<cv::Point> >  contours;
+
+    int minRed = 0;
+    int maxBlue, maxGreen = 255;
+    const int WHITE = 255;
+    const int BLACK = 0;
+
+    cv::Mat redImg(4044, 3570, CV_8UC3);
+
+    cv::Mat result(4044, 3570, CV_8UC1);
+
+    for(int y = 0; y < out.rows; y++) {
+        for(int x = 0; x < out.cols; x++) {
+            cv::Vec3b pixel = out.at<cv::Vec3b>(y,x);
+            if((pixel[2] >= minRed) /*and (pixel[0] < maxBlue) and (pixel[1] < maxGreen)*/) {
+                result.at<short>(y, x) = WHITE;
+                //std::cout << "Hi"<< std::endl;
+            }
+            else {
+                result.at<short>(y, x) = BLACK;
+            }
+        }
+    }
+
+    //cv::threshold(result, redImg, 10, 255, cv::THRESH_TOZERO_INV);
+   // cv::threshold(result,result, 0, , CV_THRESH_TOZERO);
+    //std::cout << result;
+
+    //cv::Point offset = cv::Point();
+
+    // cv::findContours(redImg, contours,
+    //                CV_RETR_ExTERNAL, CV_CHAIN_APPROx_NONE,
+    //              offset);
+
+
+    //cv::Scalar colour = cv::Scalar::all(WHITE);
+    //cv::drawContours(out, contours, -1, colour);
+
+    cv::imwrite("outImg.tif", result);
 }
 
 
