820eeb79c67b2523eb536e14c760b5ad97753fb9
pwa <pwa@pwa.(none)>
2012-06-02 23:14:47 +0200

    thresholding & contours works

--- Application.cpp
+++ Application.cpp
@@ -2,8 +2,11 @@
 #include <iostream>
 #include <vector>
 
-int HEIGHT = 400;
-int WIDTH = 400;
+int HEIGHT = 7139;
+int WIDTH = 8087;
+
+int THRESH_HEIGHT = 400;
+int THRESH_WIDTH = 400;
 
 Application::Application(int argc, char *argv[])
 {
@@ -26,7 +29,7 @@ Application::Application(int argc, char *argv[])
     //cv::namedWindow("Main");
 
 
-    cv::Mat out(cv::Size(8087, 7139), CV_8UC3);
+    cv::Mat out(cv::Size(WIDTH, HEIGHT), CV_8UC3);
 
     std::cout << out.rows << ", ";
     std::cout << out.cols << std::endl;
@@ -40,60 +43,61 @@ Application::Application(int argc, char *argv[])
     }
 
 
-    cv::imwrite("rgb.tif", out);
+    //cv::imwrite("rgb.tif", out);
 
-    /*
+
     std::vector<std::vector<cv::Point> >  contours;
 
-    int minRed = 0;
-    int maxBlue, maxGreen = 255;
-    const short WHITE = 255;
-    const short BLACK = 0;
+    const unsigned char minRed = 0;
+    const unsigned char maxBlue = 100;
+    const unsigned char maxGreen = 100;
+    const unsigned char WHITE = 255;
+    const unsigned char BLACK = 0;
 
-    cv::Mat redImg(cv::Size(4044, 3570), CV_8UC3);
 
-    cv::Mat result(cv::Size(4044, 3570), CV_8UC1);
+    cv::Mat result(cv::Size(WIDTH, HEIGHT), CV_8UC1);
 
     cv::Vec3b pixel;
     std::cout << out.rows << ", ";
     std::cout << out.cols << std::endl;
 
-    for(int x = 0; x < out.cols;  x++) {
-        for(int y = 0; y < out.rows; y++) {
+    for(int x = 0; x < WIDTH;  x++) {
+        for(int y = 0; y < HEIGHT; y++) {
             pixel = out.at<cv::Vec3b>(y,x);
-            if(pixel[2] >= minRed /*and (pixel[0] < maxBlue) and (pixel[1] < maxGreen)) {
-                result.at<short>(y,x) = WHITE;
+            if( (pixel[2] >= minRed) and (pixel[0] < maxBlue) and (pixel[1] < maxGreen)) {
+                result.at<unsigned char>(y,x) = WHITE;
                 //std::cout << (short) pixel[2] << std::endl;
             }
             else {
-                result.at<short>(y,x) = BLACK;
+                result.at<unsigned char>(y,x) = BLACK;
             }
         }
     }
 
+    cv::imwrite("outImg.tif", result);
+
     //cv::threshold(result, redImg, 10, 255, cv::THRESH_TOZERO_INV);
-    // cv::threshold(result,result, 0, , CV_THRESH_TOZERO);
+    //cv::threshold(result,result, 0, , CV_THRESH_TOZERO);
     //std::cout << result;
 
-    //cv::Point offset = cv::Point();
+    cv::Point offset = cv::Point();
 
-    // cv::findContours(redImg, contours,
-    //                CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE,
-    //              offset);
+    cv::findContours(result, contours,
+                     CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE,
+                     offset);
 
 
     //cv::Scalar colour = cv::Scalar::all(WHITE);
-    //cv::drawContours(out, contours, -1, colour);
+    cv::drawContours(out, contours, -1, cv::Scalar::all(WHITE));
 
-    cv::imwrite("outImg.tif", result);
     cv::imwrite("rgb.tif", out);
-    cv::imwrite("rgb.tif", out);
-*/}
+    //cv::imwrite("rgb.tif", out);
+}
 
 
 
 int main(int argc, char *argv[]) {
     Application foo (argc, argv);
-    std::cin.get();
+    //std::cin.get();
     return 0;
 }
