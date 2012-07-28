d4d6491f6c36d24a2a4e5c80b311e76448ed8c7a
Willi <willi@jups42.de>
2012-07-28 11:40:36 +0200

    Messed up image merge code with CPU clock measurements to prevent optimizations

--- lib/imagemerge.py
+++ lib/imagemerge.py
@@ -2,18 +2,31 @@
 
 import cv
 import sys
+import logging
+import time
 
 def load_images(images):
 	return [cv.LoadImage(img, 0) for img in images]
 
 
 def merge_images(img0, img1, img2):
+	logger = logging.getLogger('vegetation')
+
+	t0 = time.clock()
 	images = load_images([img0, img1, img2])
+	logger.info("Image loading took " + str(time.clock() - t0))
 	for i in images:
 		assert i.depth == cv.IPL_DEPTH_8U
+
+	t1 = time.clock()
 	images = resize(images)
+	logger.info("Image resizing took " + str(time.clock() - t1))
+
 	mergedImage = cv.CreateImage(min_size(images), cv.IPL_DEPTH_8U, 3)
+	t2 = time.clock()
 	cv.Merge(images[0], images[1], images[2], None, mergedImage)
+	logger.info("Image merging took " + str(time.clock() - t2))
+
 	return mergedImage
 
 
@@ -32,7 +45,11 @@ def min_size(images):
 
 
 def merge_to_file(img0, img1, img2, filePath):
-	cv.SaveImage(filePath, merge_images(img0, img1, img2))
+	img = merge_images(img0, img1, img2)
+	t0 = time.clock()
+	cv.SaveImage(filePath, img)
+	logging.getLogger('vegetation').info("Saving merged image took " +
+			str(time.clock() - t0))
 
 if __name__ == '__main__':
 	merge_images(*sys.argv[1:])
