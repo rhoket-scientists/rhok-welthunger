d6239a21adc250d5db07bcbad7ee4d0a211319d7
Willi <willi@jups42.de>
2012-08-01 16:21:12 +0200

    renamed grassy/non_grass fixtures for clearity

--- test/analysis_strategy_test.py
+++ test/analysis_strategy_test.py
@@ -8,13 +8,20 @@ from lib import analysis_strategy
 
 class ImageMergeTest(Imagetest):
 
+	def provide_image(self):
+		return 'test/fixtures/img4x4-1.tiff'
+
+
+	def provide_image_2(self):
+		return 'test/fixtures/img4x4-2.tiff'
+
+
 	def provide_grassy(self):
-		# greyscale images that contain oil palmy pixels
-		return 'test/fixtures/grassy.tiff'
+		return self.provide_image()
 
 
 	def provide_non_grassy(self):
-		return 'test/fixtures/non_grassy.tiff'
+		return self.provide_image_2()
 
 
 	@test('should count grassy pixels')
@@ -28,9 +35,9 @@ class ImageMergeTest(Imagetest):
 
 
 	@test("should compute NDVI as (Band4 - Band3) / (Band4 + Band3) and scale it to (ndvi+1)*100")
-	def _(self, grassy, non_grassy):
-		band3 = cv.LoadImage(grassy, 0)
-		band4 = cv.LoadImage(non_grassy, 0)
+	def _(self, image, image_2):
+		band3 = cv.LoadImage(image, 0)
+		band4 = cv.LoadImage(image_2, 0)
 		band4_px = band4[0, 0]
 		band3_px = band3[0,0]
 		ok(band3_px) == 75.0
@@ -43,8 +50,8 @@ class ImageMergeTest(Imagetest):
 
 
 	@test("should produce histogram as a python list")
-	def _(self, grassy):
-		img = cv.LoadImage(grassy, 0)
+	def _(self, image):
+		img = cv.LoadImage(image, 0)
 		n_buckets = 4
 		h = analysis_strategy.ndvi_histogram(img, n_buckets)
 		ok (h[0]) == 6
